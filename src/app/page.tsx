d4e8c181f57371645f0ed3e998382bba24067c6d
hiromitsusasaki <hiromitsu.sasaki@gmail.com>
2023-08-05 06:54:41 +0900

    feat: get name of spreadsheet from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ import 'tailwindcss/tailwind.css'
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [name, setName] = useState('...Loading' as string);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get(
         `${process.env.NEXT_PUBLIC_GAS_API_URL}action=getSheetNames`,
       )
-      setData(result.data);
+      setData(result.data.sheets);
+      setName(result.data.name);
     }
     fetchData();
   }, []);
@@ -20,7 +22,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center flex-1 px-5 sm:px-20 text-center">
-        <h1 className="text-2xl sm:text-6xl font-bold">学科まとめ</h1>
+        <h1 className="text-2xl sm:text-6xl font-bold">{name}</h1>
         <div className="flex flex-wrap justify-center">
           {data.map((item, index) => (
             <div key={index} className="m-4 p-6 text-lg border border-gray-300 rounded-lg">
